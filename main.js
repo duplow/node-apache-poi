064a4d7ace8bab49ef1ed0dc292efbbecf93d97b
William Wagner <william.wagner.br+1@gmail.com>
2022-01-22 10:19:55 -0300

    Updates

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log('ok')
+//import * as poi from './build/Release/obj/poi-binding';
+import java from 'java';
+java.classpath.push('../poi-binding/src/poi-bin-5.2.0/poi-5.2.0.jar')
+var myClass = java.newInstanceSync('api.model.MyObject');
+
+console.log({ myClass })
